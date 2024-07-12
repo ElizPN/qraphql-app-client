2a0f33d7c63d1d8304cb684072bb4ec15ee4749b
ElizPN <pankevych.liza@gmail.com>
2024-07-12 14:11:22 +0200

    initial states and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" />
+        <input type="number" />
+        <div className="btns">
+          <button >Создать</button>
+          <button >Получить</button>
+        </div>
+      </form>
+      <div>
+        {users.map(user =>
+          <div className="user">{user.id}. {user.username} {user.age}</div>
+        )}
+      </div>
     </div>
   );
 }
